1d5be065bfa929b580ad46a99f64c86f256ed42f
eersels3764 <ivo.eersels@idewe.be>
2020-08-28 14:43:12 +0200

    Initial Project Setup as generated by Spring Initializer

---
+++ src/test/java/guru/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
