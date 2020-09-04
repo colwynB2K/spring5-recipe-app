fa3b3d626726513778966c6d00d514913c0ed8f5
eersels3764 <ivo.eersels@idewe.be>
2020-09-04 11:26:10 +0200

    Spring Data JPA Query Methods

--- src/main/java/guru/springframework/spring5recipeapp/controller/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controller/IndexController.java
@@ -1,14 +1,7 @@
 package guru.springframework.spring5recipeapp.controller;
 
-import guru.springframework.spring5recipeapp.domain.Category;
-import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
-import guru.springframework.spring5recipeapp.repository.CategoryRepository;
-import guru.springframework.spring5recipeapp.repository.UnitOfMeasureRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
