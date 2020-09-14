d6df0e0c17980dfa851b24cb1ef533b261d92e83
eersels3764 <ivo.eersels@idewe.be>
2020-09-14 15:17:27 +0200

    Use Mockito Argument Capture to verify called method arguments

--- src/test/java/guru/springframework/spring5recipeapp/controller/IndexControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controller/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package guru.springframework.spring5recipeapp.controller;
 
+import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anySet;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -34,13 +37,27 @@ class IndexControllerTest {
     void getIndexPage() {
         // given
         String expectedResult = "index";
+        Set<Recipe> expectedRecipes = new HashSet<>();
+        Recipe expectedRecipe1 = new Recipe();
+        expectedRecipe1.setName("BLAAT");
+
+        Recipe expectedRecipe2 = new Recipe();
+        expectedRecipe2.setName("MEKKER");
+
+        expectedRecipes.add(expectedRecipe1);
+        expectedRecipes.add(expectedRecipe2);
+        when(mockRecipeService.findAll()).thenReturn(expectedRecipes);
+
+        ArgumentCaptor<Set<Recipe>> recipeSetArgumumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         // when
         String actualResult = indexController.getIndexPage(mockModel);
 
         // then
         verify(mockRecipeService, times(1)).findAll();
-        verify(mockModel, times(1)).addAttribute(eq("recipes"),anySet());
+        verify(mockModel, times(1)).addAttribute(eq("recipes"), recipeSetArgumumentCaptor.capture());
+        Set<Recipe> actualRecipes = recipeSetArgumumentCaptor.getValue();
+        assertEquals(2, actualRecipes.size());
         assertEquals(expectedResult, actualResult);
     }
 }
