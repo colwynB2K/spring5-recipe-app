d256cefa6889c8e6bfcf7d83f73d60be0347aa8c
eersels3764 <ivo.eersels@idewe.be>
2020-12-23 15:40:11 +0100

    Configure MySQL ---------------
    	* Setup 2 databases:
    		sfg_dev
    		sfg_prod
    
    	Using MYSQL root user with your application is BAD PRACTICE AND SECURITY RISK => SQL injection can destroy database as this account has GOD rights!!!
    	Best practive = use service account with minimum authority required
    
    	* Setup 2 service accounts:
    		sfg_dev_user
    		sfg_prod_user
    
    		Both should have DML access (basic CRUD) to their respective databases
    			DML = Data Manipulation Language (SQL statements which do changes on the data INSERT, UPDATE, etc...)
    		Neither should have DDL access
    			DDL = Data Definition Language (type SQL used to define tables, columns, indexes, foreign keys, etc...)
    
    	* SOLUTION:
    		## Use to run mysql db docker imqge, optionql if you're not using q local mysqldb' ||
    		# docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yep -d mysql
    		# docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yep -v C:\tools\dockerdata\mysql:/var/lib/mysql -d mysql
    
    		# Connect to mysql and run as root user
    		# Create Databases
    		CREATE DATABASE sfg_prod;
    		CREATE DATABASE sfg_prod;
    
    		# Create database service accounts
    		CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'devguru';
    		# When using Docker Containers you will need to allow connection from other hosts (as you will be connecting from outsidde the container), % is a wildcard meaning ANY in SQL
    		CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'devguru';
    		CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'prodguru';
    		CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'prodguru';
    
    		# Database grants
    		GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
    		GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
    		GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
    		GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
    		GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
    		GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
    		GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
    		GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
    		GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
    		GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
    		GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
    		GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
    		GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
    		GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
    		GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
    		GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,33 @@
+## Use to run mysql db docker imqge, optionql if you're not using q local mysqldb' ||
+# docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yep -d mysql
+# docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yep -v C:\tools\dockerdata\mysql:/var/lib/mysql -d mysql
+
+# Connect to mysql and run as root user
+# Create Databases
+CREATE DATABASE sfg_prod;
+CREATE DATABASE sfg_prod;
+
+# Create database service accounts
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'devguru';
+# When using Docker Containers you will need to allow connection from other hosts (as you will be connecting from outsidde the container), % is a wildcard meaning ANY in SQL
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'devguru';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'prodguru';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'prodguru';
+
+# Database grants
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
